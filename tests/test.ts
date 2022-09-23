3b6d3ec7eb4c3262cc42a76ffc5d32758f847e2b
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2022-09-23 10:16:29 -0400

    chore: fixe example  in test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+	const h1 = 'Welcome to the LJ Dev -- SvelteKit Typescript Tailwind Pug Starter.';
+	await expect(page.locator('h1')).toHaveText(h1);
 });
