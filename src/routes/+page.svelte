13569abb1c6d09fb64d52b6eb0d0212e29044644
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2022-09-23 10:16:13 -0400

    chore: update welcome message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
 
 <template lang="pug">
 .w-full.flex.justify-center.h-screen.items-center
-  h1.text-blue-500 Welcome to The SvelteKit Typescript Tailwind Pug Starter.
+  h1.text-blue-500 Welcome to the LJ Dev -- SvelteKit Typescript Tailwind Pug Starter.
 </template>
