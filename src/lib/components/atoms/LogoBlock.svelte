5caf9ea546906ae9e6aab2be592e350ea868690d
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2022-09-26 17:19:35 -0400

    minor tweak to component

--- src/lib/components/atoms/LogoBlock.svelte
+++ src/lib/components/atoms/LogoBlock.svelte
@@ -16,12 +16,13 @@
 a("{href}" "{title}")
 	figure.text-center.text-sm
 		img.block.w-full.h-auto(
-			alt!="{name + 'logo'}"
-			height="160" width="160"
-			src!="{'/images/' + name.toLowerCase() + '.svg'}")
-		figcaption.hidden.border-t.border-white.pt-4.border-opacity-40.mt-1.font-light.transition-colors(
-			class="sm:block lg:text-base xl:text-lg tracking-wider group-hover:text-blue-300"
+			alt!="{name + ' logo'}"
+			height="160"
+			src!="{'/images/' + name.toLowerCase() + '.svg'}"
+			width="160"
 			)
+		figcaption.hidden.border-t.border-white.pt-4.border-opacity-40.mt-1.font-light.transition-colors.tracking-wider(
+			class="sm:block lg:text-base xl:text-lg group-hover:text-blue-300")
 			| {name}
 
 +if('!isLast')
