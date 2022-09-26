979216a553704dd52e65010df8a24e901a0e2cff
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2022-09-26 16:36:18 -0400

    chore: comment tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
+				// custom rules for starter web page, remove these for your own project
 				primary: '#142239',
 				accent: '#ebf92f'
 			}
