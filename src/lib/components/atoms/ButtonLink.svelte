0e8599d5fd6d7ace7e029f33446f309427fbe553
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2022-12-11 07:36:02 -0500

    chore: refactor and supplement documentation

--- src/lib/components/atoms/ButtonLink.svelte
+++ src/lib/components/atoms/ButtonLink.svelte
@@ -6,7 +6,7 @@ An <a> styled to look like a button.
 * slot (default) -- link text
 -->
 <script lang="ts">
-	export let url = '#';
+	export let targetUrl = '#';
 	export let title = '';
 </script>
 
@@ -14,7 +14,7 @@ An <a> styled to look like a button.
 .mb-16.w-full(class="sm:mb-24")
   a.inline-block.px-4.py-2.text-light.bg-transparent.outline.outline-white.outline-1.rounded.transition-all(
     class="hover:bg-accent hover:outline-transparent hover:text-primary"
-    href="{url}"
+    href="{targetUrl}"
     title="{title}"
     )
     slot
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,22 @@
 <!-- Example Svelte Page / Starter Web Page-->
 <script lang="ts">
 	// components
-	import LogoBlock from '$components/molecules/LogoBlock.svelte';
 	import ButtonLink from '$atoms/ButtonLink.svelte';
 	import Footer from '$atoms/Footer.svelte';
+	import LogoBlock from '$components/molecules/LogoBlock.svelte';
 	import LogosGrid from '$molecules/LogosGrid.svelte';
 	import PreFooter from '$atoms/PreFooter.svelte';
 
 	// store
 	import { brands } from '$stores/brandsStore';
+
+	// logos to display
+	$brands = [
+		{ name: 'Svelte', url: 'https://svelte.dev/' },
+		{ name: 'Typescript', url: 'https://www.typescriptlang.org/' },
+		{ name: 'TailwindCSS', url: 'https://tailwindcss.com/' },
+		{ name: 'Pug', url: 'https://pugjs.org/api/getting-started.html' }
+	];
 </script>
 
 <template lang="pug">
@@ -35,21 +43,22 @@ main.grid.place-content-center.min-h-screen.text-white.p-4.relative.pb-48.bg-pri
 
 			//- cta
 			ButtonLink(
-				url!="https://github.com/lightning-jar/lj-sveltekit-ts-tailwind-pug-starter"
-				title!="View project on Github"
-				) Clone from Github
+				targetUrl="https://github.com/lightning-jar/lj-sveltekit-ts-tailwind-pug-starter"
+				title="View project on Github"
+				)
+				| Clone from Github
 
 //- pre-footer
 PreFooter(
-	targetUrl!="https://pugify.dev"
-	title!="convert HTML to pug"
+	targetUrl="https://pugify.dev"
+	title="convert HTML to pug"
 	)
 	svelte:fragment(slot="default") Also check out the ad-free HTML to Pug converter @ https://pugify.dev
 
 //- footer
 Footer(
-	authorLink!="https://fosstodon.org/@kevinpeckham"
-	orgLink!="https://lightningjar.com"
+	authorLink="https://fosstodon.org/@kevinpeckham"
+	orgLink="https://lightningjar.com"
 	)
 	svelte:fragment(slot="author") Kevin Peckham
 	svelte:fragment(slot="org") Lightning Jar
