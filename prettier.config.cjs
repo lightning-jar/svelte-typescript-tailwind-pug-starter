a618a2f5565f9f9d733820e7dffb1f7ac8a94b0c
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-01-12 20:52:28 -0500

    adding prettier-plugins

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,33 @@
+const prettierPluginSvelte = require('prettier-plugin-svelte');
+const prettierPluginPug = require('@prettier/plugin-pug');
+const prettierPluginTailwindcss = require('prettier-plugin-tailwindcss');
+const prettierPluginOrganizeImports = require('prettier-plugin-organize-imports');
+
+// @ts-check
+/// <reference types="./src/prettier" />
+/// <reference types="@prettier/plugin-pug/src/prettier" />
+
+/**
+ * @type {import('prettier').Options}
+ */
 module.exports = {
-	plugins: [require('prettier-plugin-tailwindcss')],
-	tailWindConfig: './tailwind.config.cjs'
+	plugins: [prettierPluginSvelte, prettierPluginPug, prettierPluginTailwindcss],
+	bracketSpacing: true,
+	useTabs: true,
+	singleQuote: false,
+	printWidth: 100,
+	semi: true,
+	tabWidth: 2,
+	quoteProps: 'consistent',
+	arrowParens: 'always',
+	trailingComma: 'all',
+	pluginSearchDirs: false,
+	pugTabWidth: 2,
+	pugUseTabs: 'true',
+	pugSortAttributes: 'asc',
+	pugWrapAttributes: 'none',
+	pugFramework: 'svelte',
+	pugEmptyAttributesThreshold: 1,
+	pugClassNotation: 'literal',
+	pugIdNotation: 'literal'
 };
