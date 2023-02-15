102f23d6cc1cebcfeb793541987deed5f4f873ed
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-02-15 07:08:35 -0500

    bug: fix example error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	// sveltekit error page documentation here:
+	// https://kit.svelte.dev/docs/routing#error
+
+	// import from svelte
+	import { page } from "$app/stores";
+</script>
+
+<template lang="pug">
+	.text-36.grid.h-screen.w-screen.grid-cols-1.place-content-center.place-items-center.bg-primary.text-white
+		div {  $page.status  }: {  $page.error.message  }
+</template>
