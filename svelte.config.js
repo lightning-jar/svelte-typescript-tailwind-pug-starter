b3870fe42681c05e0dab157073040c06893960d7
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-06-15 09:03:36 -0400

    config: use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+import vercel from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,13 +8,20 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [preprocess()],
 	kit: {
-		adapter: adapter(),
+		adapter: vercel({
+			// make explicit -- vercel does not yet support later node versions
+			runtime: "nodejs18.x",
+		}),
 		alias: {
 			$atoms: "./src/lib/components/atoms",
+			$assets: "./src/lib/assets",
 			$components: "./src/lib/components",
+			$data: "./src/lib/data",
 			$molecules: "./src/lib/components/molecules",
 			$organisms: "./src/lib/components/organisms",
+			$settings: "./src/lib/settings",
 			$stores: "./src/lib/stores",
+			$tools: "./src/lib/components/tools",
 			$types: "./src/lib/types",
 			$utils: "./src/lib/utils",
 		},
