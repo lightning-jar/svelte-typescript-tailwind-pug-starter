892421453081df3cadce3de55e822901f6441949
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-05-02 07:31:00 -0400

    update custom types

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,15 +1,15 @@
 // a great place to put some custom types
 interface Link {
 	[key: string]: unknown;
-	label: string;
-	href: string;
+	label?: string;
+	href?: string;
 	rel?: string;
 	target?: string;
 	title?: string;
 }
 interface Image {
 	[key: string]: unknown;
-	src: string;
+	src?: string;
 	alt?: string;
 	loading?: string;
 	height?: string;
