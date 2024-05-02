4be8e4b44b4c535eea60425f1e833b1c0478d89f
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2024-05-02 07:23:42 -0400

    add custom type examples

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,1 +1,17 @@
 // a great place to put some custom types
+interface Link {
+	[key: string]: unknown;
+	label: string;
+	href: string;
+	rel?: string;
+	target?: string;
+	title?: string;
+}
+interface Image {
+	[key: string]: unknown;
+	src: string;
+	alt?: string;
+	loading?: string;
+	height?: string;
+	width?: string;
+}
